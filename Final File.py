2fc8e716a5379f892a1e3323fcb1b342a2cfb983
Yuming Guo <yumingguo2002@gmail.com>
2019-09-03 12:29:50 +0100

    0901 V0.1.2
    Adds a tank which can be moved

--- Final File.py
+++ Final File.py
@@ -2,11 +2,28 @@ import pygame
 
 pygame.init() #initiates pygame
 
-gameDisplay = pygame.display.set_mode((1440,1080)) #sets resolution
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
 
-pygame.display.set_caption("Tank 19 V0.1.1") #sets title
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height)) #sets resolution
+
+pygame.display.set_caption('Tank 19 V0.1.2') #sets title
 
 clock = pygame.time.Clock() #sets clock speed
+
+
+tankImg = pygame.image.load('D:/Schools/3 Dulwich College/3 Computer Science/Pygame Project/Images/Tank.jpg')
+def tank(x,y):
+    gameDisplay.blit(tankImg, (x,y))
+x = display_width * 0.5
+y = display_height * 0.8
+x_change = 0
+y_change = 0
 
 done = False #initiate game loop
 
@@ -15,9 +32,25 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done= True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+            elif event.key == pygame.K_UP:
+                y_change = -5
+            elif event.key == pygame.K_DOWN:
+                y_change = 5
 
-        print(event)
+        if event.type == pygame.KEYUP:
+            x_change = 0
+            y_change = 0
 
+    x = x + x_change
+    y = y + y_change
+
+    gameDisplay.fill(white)
+    tank(x,y)
     pygame.display.update()
     clock.tick(30)
 
